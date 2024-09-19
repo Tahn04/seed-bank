b1b9a6bad4da1baf5e55b9f76f1c01b6b6e24462
Tahn Jandai <taja6898@colorado.edu>
2024-09-19 00:50:07 -0600

    Routing working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { MainPageComponent } from "./main-page/main-page.component";
+import { SeedCatalogComponent } from "./seed-catalog/seed-catalog.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, MainPageComponent],
+  imports: [RouterModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, MainPageComponent, SeedCatalogComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { DevPageComponent } from './dev-page/dev-page.component';  
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainPageComponent, 
+        title: 'Main Page'
+    }, 
+    {
+        path: 'dev',
+        component: DevPageComponent,
+        title: 'Developer Page'
+    }
+];
